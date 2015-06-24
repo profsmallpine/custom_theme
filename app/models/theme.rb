95ae259ebfd4b68295c9d06f7d60cddf9521748b
Joe Timmer <profsmallpine@gmail.com>
2015-06-24 08:58:56 -0600

    Subdomain is selecting which label the store is on, and then grabbing values from a yaml file for that specific subdomain.

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -2,4 +2,8 @@ class Theme < ActiveRecord::Base
   belongs_to :tenant
 
   default_scope {where(tenant_id: Tenant.current_id)}
+
+  def self.return_whitelabel
+    Whitelabel.label.some_config
+  end
 end
